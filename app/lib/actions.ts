827d1818dc93ca4b8efd456710dffef5b1690373
Matheus Porto <matheus.porto@sap.com>
2025-08-16 17:58:03 -0300

    Chapter 12 - Add create invoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string()
+});
+
+const CreateInvoice = FormSchema.omit({
+  id: true, 
+  date: true,
+});
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // Next.js has a client-side router cache that stores the route segments in the user's browser for a time.
+  // Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server.
+  // You can do this with the revalidatePath function from Next.js:
+  revalidatePath('/dashboard/invoices');
+
+  // Then we already created the new invoice and revalidated the path, so we can redirect the user to invoices page.
+  redirect('/dashboard/invoices');
+}
