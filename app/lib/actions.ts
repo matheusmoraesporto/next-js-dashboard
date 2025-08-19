e87a8cc4fa1bac9bc0c245aef3dfaa024c8a1bdf
Matheus Porto <matheus.porto@sap.com>
2025-08-18 21:11:46 -0300

    Chapter 13 - Handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,74 +1,88 @@
 "use server";
 
-import { z } from 'zod';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-import postgres from 'postgres';
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string()
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({
-  id: true, 
+  id: true,
   date: true,
 });
 
 const UpdateInvoice = FormSchema.omit({
-  id: true, 
+  id: true,
   date: true,
 });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
   });
 
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
+  const date = new Date().toISOString().split("T")[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.log("Error creating invoice:", error);
+  }
 
   // Next.js has a client-side router cache that stores the route segments in the user's browser for a time.
   // Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server.
   // You can do this with the revalidatePath function from Next.js:
-  revalidatePath('/dashboard/invoices');
+  revalidatePath("/dashboard/invoices");
 
   // Then we already created the new invoice and revalidated the path, so we can redirect the user to invoices page.
-  redirect('/dashboard/invoices');
+  redirect("/dashboard/invoices");
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
   });
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    console.log("Error updating invoice:", error);
+  }
 
   revalidatePath(`/dashboard/invoices`);
   redirect(`/dashboard/invoices`);
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  throw new Error("Failed to Delete Invoice");
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.log("Error deleting invoice:", error);
+  }
+  revalidatePath("/dashboard/invoices");
 }
